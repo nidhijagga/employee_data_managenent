bba0e1029ae414891b55a74f62267a6624c8dc66
nidhijaggaziprock <nidhi.jagga@ziprocktechnologies.com>
2024-06-22 16:18:43 +0530

    Given Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl underline">
-        Hello world!
-      </h1>
+    <div className="App bg-gray-50 min-h-screen p-6">
+      <div className="flex items-center justify-between bg-white p-4 shadow-md rounded-lg">
+        <h1 className="text-3xl font-bold text-gray-800">
+          Employee Database Management
+        </h1>
+        <button className="bg-slate-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-slate-100 hover:text-slate-800 transition duration-300">
+          Add Employee
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/EmployeeInformation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EmployeeInformation = () => {
+  return (
+    <div>EmployeeInformation</div>
+  )
+}
